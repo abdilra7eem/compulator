378f903f880acdd20e3dea9631cb8f36087b050f
Abdalrahim Fakhouri <afakhouri@localhost.localdomain>
2020-02-22 21:12:45 +0200

    The calculator works properly, but the keyboard events are still broken

--- src/Compulator.js
+++ src/Compulator.js
@@ -1,15 +1,16 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './reset.css';
 import './compulator.css';
 
+  // The 'Button' component is a template for each individual button
 function Button(props) {
-  let myid = (props.value === '.') ? 'btndot': props.myid; 
-  let btnstyle = {gridArea: myid}
+  let myid = (props.value === '.') ? 'btndot': props.myid; // CSS grids did not accept "btn."
   return(
-    <button onClick={() => {props.clickHandler(props.value)}} id={myid} style={btnstyle}>{props.value}</button>
+    <button onClick={() => {props.clickHandler(props.value)}} id={myid} style={{gridArea: myid}}>{props.value}</button>
   )
 }
 
+// The 'Buttons' component creates buttons based on the "Button" template an array of elements
 function Buttons(props){
   if (props.type==="nums"){
     return(
@@ -24,6 +25,7 @@ function Buttons(props){
   }
 }
 
+// the Screen component resembles the screen of the calculator
 function Screen(props){
   return(
     <div id='screen'>
@@ -32,48 +34,115 @@ function Screen(props){
   )
 }
 
+
+
+// Compulator is the main calculator component
 function Compulator() {
+
+  // Create arrays for the buttons. 'num' represents numbers, and 'ops' represents operations
   let nums = Array.from({length: 10}, (v, i) => i);
   nums.push('.');
   let ops = '=+-*/'.split('');
   ops.push('AC');
 
-  const [screen, setScreen] = useState(0);
-  const [memory, setMemory] = useState(0);
-  const [op, setOp] = useState('');
+  // State variables needed for the calculator
+  const [screen, setScreen] = useState(0); // the screen content
+  const [memory, setMemory] = useState(0); // the memory contains the previous number
+  const [op, setOp] = useState(''); // the previous operator; it's used when the next operator button is pressed
 
+  // Called by the number buttons
   const numClick = (value) => {
     if (op === '' || op === 'AC'){
-      let newScreen = `${screen}${value}`.replace(/^0+/, '');
-      newScreen = newScreen.startsWith('.') ? "0"+newScreen: newScreen;
-      setScreen(newScreen);
-    }else{
+      if (!(value==='.' && (screen.toString()).includes('.'))){//Avoid multiple decimal separators
+        let newScreen = `${screen}${value}`.replace(/^0+/, ''); // Remove leading zeros
+        newScreen = newScreen.startsWith('.') ? "0"+newScreen: newScreen; // Adds a leading zero if 'screen' starts with a decimal separator
+        setScreen(newScreen);
+      }
+    }else if (value !== '.'){
       setScreen(value);
-      setOp('');
     }
   }
 
-  const opClick = (value) => {
-    if (value === 'AC'){
-        setMemory(0);
-        setScreen(0);
-    }else if (value ==='='){
-        if(op === '' || op === '='){
-          
-        }
-        let result = parseFloat(`${memory} ${op} ${screen}`);
-        console.log(`result is ${result}`);
-        setMemory(result);
-        setScreen(result);
-      } else{
-        let result = parseFloat(`${memory} ${op} ${screen}`);
-        console.log(`result is ${result}`);
-        setMemory(result);
-      }
+  // 'calculate' evaluates the operator and does the calculation
+  // parseFloat is used because JS is weakly typed.
+  const calculate = ()=>{
+    let result = 0;
+    switch(op){
+      case ('+' || '='):
+        result = parseFloat(memory) + parseFloat(screen);
+        break;
+      case '-':
+        result = parseFloat(memory) - parseFloat(screen);
+        break;
+      case '*':
+        result = parseFloat(memory) * parseFloat(screen);
+        break;
+      case '/':
+        result = parseFloat(memory) / parseFloat(screen);
+        break;
+      default:
+        console.trace('Unexpected error occured');
+    }
 
-    setOp(value);
+    console.log(`result = ${result}`);
+    return(result);
   }
 
+  // Called by operation buttons (+, -, *, /, =, AC)
+  const opClick = (value) => {
+
+    console.log('opClick: '+value)
+
+    // 'op' is the previous operator, 'value' is the currently pressed button operator
+    if (value === 'AC'){
+      // When 'AC' is pressed, both 'memory' and 'screen' are reset.
+        setMemory(0);
+        setScreen(0);
+    }else if(op === '' || op === '=' || op === 'AC'){
+      // If there is no previous operator, no calculation will be done
+      setMemory( value==='=' ? 0 : screen ) // the memory will be reset when '=' is pressed
+    } else{
+      // The code to be run if there was a previous operator
+      let result = calculate();
+      if (isNaN(result)){ // to avoid NaN values (Happens when multiplying infinity by zero)
+        result=0;
+      }
+      setMemory(result);
+      setScreen(result);
+    }
+
+    setOp(value); // set 'op' to the currently pressed operator button
+  }
+
+  // KeyboardEvents: still broken
+  function KBNumPad(key){
+    useEffect(() => {
+      window.addEventListener("keydown", onDown)
+      return () => {
+          window.removeEventListener("keydown", onDown)
+      }
+    }, [key])
+  
+    const onDown = event => {
+      console.log(event);
+      if((event.key).toString() === 'Enter'){
+        // console.log((event.key).toString() + " is Enter")
+        opClick('=');
+      }
+
+      if((nums.toString()).includes(event.key)){
+        // console.log((event.key).toString() + " is a number")
+        numClick((event.key).toString());
+      }
+      
+      if((ops.toString()).includes(event.key)){
+        // console.log((event.key).toString() + " is an operator")
+        opClick((event.key).toString());
+      }
+    }
+  }
+  KBNumPad();
+  
   return (
     <div id="Compulator">
       <Screen value={screen}/>
